99826c874a063e1942fd2e903d8d1974fce34bbd
carloseduribeiro <carloseribeiro96@gmail.com>
2024-02-20 22:49:32 -0300

    Adding database migration script to create table when the docker compose is runned

---
+++ script/sql/db.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS orders;
+
+USE orders;
+
+CREATE TABLE IF NOT EXISTS orders (
+    id VARCHAR(255) NOT NULL,
+    price FLOAT NOT NULL,
+    tax VARCHAR(255) NOT NULL,
+    final_price FLOAT NOT NULL,
+    PRIMARY KEY (id)
+);
